a835a48f98059e03ff98aee2dc77d3cd05ce8ed6
IvanKarpets <ivankarpets@icloud.com>
2019-01-25 18:41:05 +0200

    Initial Commit

---
+++ Messanger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messanger
+//
+//  Created by Ivan Karpets on 1/25/19.
+//  Copyright © 2019 Ivan Karpets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
